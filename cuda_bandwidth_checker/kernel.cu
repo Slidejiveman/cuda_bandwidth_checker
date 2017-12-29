7e9396531a47e67345047823894eed9e8a729057
Slidejiveman <rdnotlaw91@outlook.com>
2017-12-29 14:43:36 -0600

    Finished with comments

--- cuda_bandwidth_checker/kernel.cu
+++ cuda_bandwidth_checker/kernel.cu
@@ -26,6 +26,15 @@ inline cudaError_t checkCuda(cudaError_t result)
 	return result;
 }
 
+// Copies the data between the device and the host. It also takes 
+// performance measurements.
+// If the transfers are small, then it would be better in a real 
+// application to do them batched transfers. You can do this by
+// using a temporary array, preferably pinned, and packing all
+// of the data that needs to be transferred into it. Transfer
+// it when ready.
+// this method can be used: (there is also a 3D version)
+// cudaMemcpy2D(dest, dest_pitch, src, src_pitch, w, h, cudaMemcpyHostToDevice)
 void profileCopies(float* h_a, float* h_b, float* d, unsigned int n, char* desc) {
 	printf("\n%s transfers\n", desc);
 
@@ -33,15 +42,97 @@ void profileCopies(float* h_a, float* h_b, float* d, unsigned int n, char* desc)
 
 	//events for timing
 	cudaEvent_t startEvent, stopEvent;
-
 	checkCuda(cudaEventCreate(&startEvent));
 	checkCuda(cudaEventCreate(&stopEvent));
 
+	// record the time it takes for the host to copy the
+	// data over to the device.
+	// Note, it's better to do this kind of analysis with
+	// nvprof or Nsight rather than instrument the code.
+	checkCuda(cudaEventRecord(startEvent, 0));
+	checkCuda(cudaMemcpy(d, h_a, bytes, cudaMemcpyHostToDevice));
+	checkCuda(cudaEventRecord(stopEvent, 0));
+	checkCuda(cudaEventSynchronize(stopEvent));
+	// print result
+	float time;
+	checkCuda(cudaEventElapsedTime(&time, startEvent, stopEvent));
+	printf("  Host to Deice bandwidth (GB/s): %f\n", bytes * 1e-6 / time);
 
+	// Do the same thing from the device back to the host.
+	checkCuda(cudaEventRecord(startEvent, 0));
+	checkCuda(cudaMemcpy(h_b, d, bytes, cudaMemcpyDeviceToHost));
+	checkCuda(cudaEventRecord(stopEvent, 0));
+	checkCuda(cudaEventSynchronize(stopEvent));
+	
+	checkCuda(cudaEventElapsedTime(&time, startEvent, stopEvent));
+	printf("  Device to Host bandwidth (GB/s): %f\n", bytes * 1e-6 / time);
+
+	for (int i = 0; i < n; i++) {
+		if (h_a[i] != h_b[i]) {
+			printf("*** %s transfers failed *** \n", desc);
+			break;
+		}
+	}
+
+	// clean up events
+	checkCuda(cudaEventDestroy(startEvent));
+	checkCuda(cudaEventDestroy(stopEvent));
 }
 
 int main()
 {
-	
+	unsigned int nElements = 4 * 1024 * 1024;
+	const unsigned int bytes = nElements * sizeof(float);
+
+	//host arrays
+	float *h_aPageable, *h_bPageable;
+	float *h_aPinned, *h_bPinned;
+
+	// device array
+	float *d_a;
+
+	// allocate and initialize
+	h_aPageable = (float*)malloc(bytes);                   // host pageable
+	h_bPageable = (float*)malloc(bytes);                   // host pageable
+	checkCuda(cudaMallocHost((void**)&h_aPinned, bytes));  // host pinned
+	checkCuda(cudaMallocHost((void**)&h_bPinned, bytes));  // host pinned
+	checkCuda(cudaMalloc((void**)&d_a, bytes));            // device
+
+	for (int i = 0; i < nElements; ++i) {
+		h_aPageable[i] = i;
+	}
+	memcpy(h_aPinned, h_aPageable, bytes);
+	memset(h_bPageable, 0, bytes);
+	memset(h_bPageable, 0, bytes);
+
+	// output device info and transfer size
+	cudaDeviceProp prop;
+	checkCuda(cudaGetDeviceProperties(&prop, 0));
+
+	printf("\nDevice: %s\n", prop.name);
+	printf("Transfer size (MB): %d\n", bytes / (1024 * 1024));
+
+	// perform copies and report bandwidth
+	profileCopies(h_aPageable, h_bPageable, d_a, nElements, "Pageable");
+	profileCopies(h_aPinned, h_bPinned, d_a, nElements, "Pinned");
+
+	printf("\n");
+
+	// cleanup
+	cudaFree(d_a);
+	cudaFreeHost(h_aPinned);
+	cudaFreeHost(h_bPinned);
+	free(h_aPageable);
+	free(h_bPageable);
+
+	// On my machine the pinned memory is over 3 times faster.
+	// This is all device dependent, however.
+	// Do not overuse Pinned Memory though. It limits the memory
+	// available to the operating system, etc. So, test to make
+	// sure the application is working suitably.
+
+	// Ultimately, take care to minimize the number of transfers
+	// and to optomize them when they must happen. This is the 
+	// bottleneck of hybrid CPU/GPU computing.
     return 0;
 }
